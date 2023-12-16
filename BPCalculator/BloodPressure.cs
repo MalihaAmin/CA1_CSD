92248294b8a254d18636e3aa6c34468e63f7c93a
Maliha Binte Ruhul Amin <maliha@Malihas-MacBook-Air.local>
2023-12-16 19:12:30 +0000

    test 14

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -49,11 +49,6 @@ namespace BPCalculator
                 {
                     return BPCategory.High;
                 }
-                else
-                {
-
-                    return BPCategory.NotValid;
-                }
             }
         }
     }
