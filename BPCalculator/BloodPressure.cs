68d099e3cfe0139af12e9f5049b0527557712545
MalihaAmin <aminmaliha15@gmail.com>
2023-12-17 18:30:00 +0000

    Unit tests added

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -32,28 +32,26 @@ namespace BPCalculator
         {
             get
             {
-                if (Systolic <= 89 && Diastolic <= 59)
+                if (Systolic < 90 && Diastolic < 60)
                 {
                     return BPCategory.Low;
                 }
-                else if ((Systolic >= 70 && Systolic <= 119) && (Diastolic >= 40 && Diastolic <= 79))
+                else if (Systolic >= 90 && Systolic <= 119 && Diastolic >= 60 && Diastolic <= 79)
                 {
                     return BPCategory.Ideal;
                 }
-                else
-                if ((Systolic >= 120 && Systolic <= 139) && (Diastolic <= 80) || (Systolic >= 81 && Systolic <= 139) && (Diastolic >= 80 && Diastolic <= 89))
+                else if ((Systolic >= 120 && Systolic <= 139) && (Diastolic >= 80 && Diastolic <= 89))
                 {
                     return BPCategory.PreHigh;
                 }
-                else
-                 if ((Systolic >= 140 && Systolic <= 190 || Diastolic >= 90 && Diastolic <= 100))
+                else if (Systolic >= 140 && (Diastolic >= 90 && Diastolic <= 100))
                 {
                     return BPCategory.High;
                 }
                 else
                 {
                     return BPCategory.Invalid;
-                }                     // replace this
+                }
             }
         }
     }
--- BPUnitTestProject/UnitTest1.cs
+++ BPUnitTestProject/UnitTest1.cs
@@ -1,9 +1,10 @@
 using BPCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BPCalculatorUnitTesting
 {
     [TestClass]
-    public class BloodPresureTests
+    public class BloodPressureTests
     {
         [TestMethod]
         public void Test_High()
@@ -12,6 +13,7 @@ namespace BPCalculatorUnitTesting
             BloodPressure bp = new BloodPressure() { Systolic = 150, Diastolic = 95 };
             Assert.AreEqual(BPCategory.High, bp.Category);
         }
+
         [TestMethod]
         public void Test_PreHigh()
         {
@@ -19,6 +21,7 @@ namespace BPCalculatorUnitTesting
             BloodPressure bp = new BloodPressure() { Systolic = 125, Diastolic = 85 };
             Assert.AreEqual(BPCategory.PreHigh, bp.Category);
         }
+
         [TestMethod]
         public void Test_Ideal()
         {
@@ -26,12 +29,60 @@ namespace BPCalculatorUnitTesting
             BloodPressure bp = new BloodPressure() { Systolic = 115, Diastolic = 70 };
             Assert.AreEqual(BPCategory.Ideal, bp.Category);
         }
+
         [TestMethod]
         public void Test_Low()
         {
-            //if Systolic = 80 and Diastolic = 80 then it should be 'Low' pressure
+            //if Systolic = 80 and Diastolic = 50 then it should be 'Low' pressure
             BloodPressure bp = new BloodPressure() { Systolic = 80, Diastolic = 50 };
             Assert.AreEqual(BPCategory.Low, bp.Category);
+        }
+
+        // Additional Tests
+
+        [TestMethod]
+        public void Test_Invalid()
+        {
+            // Test for invalid values
+            // if Systolic = 200 and Diastolic = 23, it should be 'NotValid'
+            BloodPressure bp = new BloodPressure() { Systolic = 200, Diastolic = 23 };
+            Assert.AreEqual(BPCategory.Invalid, bp.Category);
+        }
+
+        [TestMethod]
+        public void Test_Outside_High_Range()
+        {
+            // Test for values outside the high range
+            // if Systolic = 140 and Diastolic = 80, it should not be 'High'
+            BloodPressure bp = new BloodPressure() { Systolic = 140, Diastolic = 80 };
+            Assert.AreNotEqual(BPCategory.High, bp.Category);
+        }
+
+        [TestMethod]
+        public void Test_Outside_PreHigh_Range()
+        {
+            // Test for values outside the prehigh range
+            // if Systolic = 119 and Diastolic = 79, it should not be 'PreHigh'
+            BloodPressure bp = new BloodPressure() { Systolic = 119, Diastolic = 79 };
+            Assert.AreNotEqual(BPCategory.PreHigh, bp.Category);
+        }
+
+        [TestMethod]
+        public void Test_Outside_Ideal_Range()
+        {
+            // Test for values outside the ideal range
+            // if Systolic = 90 and Diastolic = 45, it should not be 'Ideal'
+            BloodPressure bp = new BloodPressure() { Systolic = 90, Diastolic = 45 };
+            Assert.AreNotEqual(BPCategory.Ideal, bp.Category);
+        }
+
+        [TestMethod]
+        public void Test_Outside_Low_Range()
+        {
+            // Test for values outside the low range
+            // if Systolic = 75 and Diastolic = 60, it should not be 'Low'
+            BloodPressure bp = new BloodPressure() { Systolic = 75, Diastolic = 60 };
+            Assert.AreNotEqual(BPCategory.Low, bp.Category);
         }
     }
 }
